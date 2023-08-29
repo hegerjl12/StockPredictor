239ae7ed90fab4b3850a367146f785bb6cf4d34e
hegerjl12 <jared.heger@gmail.com>
2023-08-29 16:25:47 -0700

    test files for different timeframes and calc is m > 10 sp > 0 fp > 0 with using the next candles open and high

--- main.py
+++ main.py
@@ -1,16 +1,53 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import pandas as pd
+import numpy as np
+import streamlit as st
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def main():
+    filename = st.selectbox('Choose your file', ('test1', 'test15.csv', 'test30.csv', 'test60.csv', 'test120.csv', 'test1D.csv'))
+    master_df = pd.read_csv(filename)
+    trimmed_df = master_df[
+        ['time', 'open', 'high', 'low', 'close', 'Momemtum', 'Slow Pressure', 'Fast Pressure']].copy()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    trimmed_df['change'] = trimmed_df['high'] - trimmed_df['open']
+
+    m_delta = [0]
+    sp_delta = [0]
+    fp_delta = [0]
+
+    for i in range(len(trimmed_df['Momemtum'])):
+
+        if i < len(trimmed_df['Momemtum']) - 1:
+            m_delta.append(trimmed_df.loc[i + 1, 'Momemtum'] - trimmed_df.loc[i, 'Momemtum'])
+            sp_delta.append(trimmed_df.loc[i + 1, 'Slow Pressure'] - trimmed_df.loc[i, 'Slow Pressure'])
+            fp_delta.append(trimmed_df.loc[i + 1, 'Fast Pressure'] - trimmed_df.loc[i, 'Fast Pressure'])
+
+    trimmed_df['m_delta'] = m_delta
+    trimmed_df['sp_delta'] = sp_delta
+    trimmed_df['fp_delta'] = fp_delta
+
+    st.dataframe(trimmed_df)
+
+    count_win = 0
+    count_lose = 0
+    wins = []
+    loses = []
+
+    for i in range(len(trimmed_df)):
+        if trimmed_df.loc[i, 'm_delta'] > 10 and trimmed_df.loc[i, 'sp_delta'] > 0 and trimmed_df.loc[i, 'fp_delta'] > 0:
+            st.write(i+1, trimmed_df.loc[i+1, 'change'])
+            if trimmed_df.loc[i+1,'change'] > 0:
+                count_win += 1
+                wins.append(trimmed_df.loc[i+1,'change'])
+            else:
+                count_lose += 1
+                loses.append(trimmed_df.loc[i+1,'change'])
+
+    st.write('CountWin: ', count_win, np.mean(wins))
+    st.write('CountLose:', count_lose, np.mean(loses))
+
+    return
+
+
+if __name__ == "__main__":
+    main()
