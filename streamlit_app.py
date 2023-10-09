4de62e0e7a60ba921b3cb38ecd258f660f5109d1
hegerjl12 <jared.heger@gmail.com>
2023-10-09 16:29:11 -0700

    db backupo

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,11 +57,11 @@ def process_data(spy_db, newData_df):
     spy_df.drop(index=spy_df.index[0], axis=0, inplace=True)
     spy_df.reset_index(inplace=True)
 
-   # while spy_df.head(0)['time'] != last_entry['time']:
-    #    spy_df.drop(index=spy_df.index[0], axis=0, inplace=True)
-        #spy_df.reset_index(inplace=True)
+    while str(spy_df.head(0)['time']) != last_entry['time']:
+        spy_df.drop(index=spy_df.index[0], axis=0, inplace=True)
+        spy_df.reset_index(inplace=True)
 
-    st.write(spy_df.head(1)['time'])
+    st.write(spy_df)
 
     return spy_df
 
