ae69dd488fbd236bf511708276a1634fc0494d8a
hegerjl12 <jared.heger@gmail.com>
2023-10-09 16:54:25 -0700

    db backupo

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,7 +63,8 @@ def process_data(spy_db, newData_df):
 
     st.write(spy_df[spy_df['time'] == last_entry['time']].index)
     matching_index = spy_df[spy_df['time'] == last_entry['time']].index
-    st.write(spy_df.loc[spy_df.index > int(matching_index)-21])
+    matching_index = matching_index.to_list
+    st.write(spy_df.loc[spy_df.index > matching_index[0]-21])
 
     return spy_df
 
