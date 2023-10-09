aa208d453df9d537048cbb8ce26b3afb21145fca
hegerjl12 <jared.heger@gmail.com>
2023-10-08 20:11:55 -0700

    buy price

--- streamlit_app.py
+++ streamlit_app.py
@@ -416,6 +416,7 @@ def main():
             st.write(candle_string)
             selected_candle_data = spy_db.get(candle_string)
             st.write(selected_candle_data)
+            copy_of_data = selected_candle_data
 
             if selected_candle_data is not None:
                 download = spy_models.get('put_dt_model.pkl')
@@ -435,7 +436,7 @@ def main():
                 st.write(predictor_df)
 
                 if new_dt.predict(predictor_df) == 1:
-                    st.write("ML Says Buy", " - ", selected_candle_data['close'], " Target: ", selected_candle_data['close']+0.5)
+                    st.write("ML Says Buy", " - ", copy_of_data['close'], " Target: ", copy_of_data['close']+0.5)
                 else:
                     st.write("ML Says Wait")
 
