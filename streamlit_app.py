26bda8437401719bdfc2bd51c8a974c914052647
hegerjl12 <jared.heger@gmail.com>
2023-10-08 19:55:35 -0700

    adjust time selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -409,7 +409,7 @@ def main():
             #     st.write("ML Says Wait")
 
             pred_date = st.date_input('Choose Date', datetime.date.today(), key='put_date')
-            pred_time = st.time_input('Choose Candle', datetime.time(7, 30), step=1800, key='put_time')
+            pred_time = st.selectbox('Choose Candle', ['06:30', '07:30', '08:30', '09:30', '10:30', '11:30', '12:30'])
             candle_string = str(pred_date) + 'T' + str(pred_time) + '-07:00'
             # td = datetime.timedelta(hours=1)
             # candle_string_prev = str(pred_date) + 'T' + str(pred_time-td) + '-07:00'
