24968f4e766bffe1ad4e8e23d8e052064a907c09
hegerjl12 <jared.heger@gmail.com>
2023-10-11 22:02:26 -0700

    add droo columns

--- streamlit_app.py
+++ streamlit_app.py
@@ -94,7 +94,7 @@ def create_call_model(db_df, winInput, drawdownInput):
     db_df['w_or_l'] = w_or_l
 
     # X_feed = db_df[db_df['w_or_l'] >= 0]
-    X = db_df.drop(['time', 'w_or_l', 'open', 'high', 'low', 'close', 'key', 'change_close_open', 'change_high_open', 'change_low_open'], axis=1).values
+    X = db_df.drop(['time', 'w_or_l', 'open', 'high', 'low', 'close', 'key', 'change_close_open', 'change_high_open', 'change_low_open', 'Fast Pressure', 'Slow Pressure', 'Momemtum'], axis=1).values
     y = db_df['w_or_l'].values
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=12, stratify=y)
