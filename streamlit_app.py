a8f45b088566ecfbb60d8c40b7c29845617ad08b
hegerjl12 <jared.heger@gmail.com>
2023-10-11 21:30:51 -0700

    remove drawdown from model creation

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,7 +82,7 @@ def create_call_model(db_df, winInput, drawdownInput):
         # if db_df.loc[i, 'm_delta'] > momentumInput and db_df.loc[i, 'sp_delta'] > spInput and db_df.loc[
         #     i, 'fp_delta'] > fpInput:
         # st.write(i+1, db_df.loc[i+1, 'change'])
-        if db_df.loc[i + 1, 'change_close_open'] > winInput and db_df.loc[i + 1, 'change_low_open'] > drawdownInput:
+        if db_df.loc[i + 1, 'change_close_open'] > winInput: # and db_df.loc[i + 1, 'change_low_open'] > drawdownInput:
             wins_drawdown.append(db_df.loc[i + 1, 'change_low_open'])
             w_or_l.append(1)
         else:
@@ -118,7 +118,7 @@ def create_put_model(db_df, winInput, drawdownInput):
     for i in range((len(db_df) - 1)):
         # if db_df.loc[i, 'm_delta'] < momentumInput and db_df.loc[i, 'sp_delta'] < spInput and \
         #         db_df.loc[i, 'fp_delta'] < fpInput:
-        if db_df.loc[i + 1, 'change_close_open'] < winInput and db_df.loc[i + 1, 'change_high_open'] < drawdownInput:
+        if db_df.loc[i + 1, 'change_close_open'] < winInput: # and db_df.loc[i + 1, 'change_high_open'] < drawdownInput:
             wins_drawdown.append(db_df.loc[i + 1, 'change_high_open'])
             w_or_l.append(1)
         else:
