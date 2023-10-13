6cd793abd9a7b31e6b415f7c9bfc44da376e08f2
hegerjl12 <jared.heger@gmail.com>
2023-10-12 22:27:51 -0700

    playing with model creation

--- streamlit_app.py
+++ streamlit_app.py
@@ -98,7 +98,7 @@ def create_call_model(db_df, winInput, drawdownInput):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=12, stratify=y)
 
     dt = DecisionTreeClassifier(max_depth=3, random_state=12)
-    clf = KNeighborsClassifier(n_neighbors=5)
+    clf = KNeighborsClassifier(n_neighbors=10)
     dt.fit(X_train, y_train)
     clf.fit(X_train, y_train)
 
