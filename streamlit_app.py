38e854b4a44a46b38c6e76865e64ee5e0fb776bf
hegerjl12 <jared.heger@gmail.com>
2023-10-12 22:21:09 -0700

    playing with model creation

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,10 +80,7 @@ def create_call_model(db_df, winInput, drawdownInput):
     w_or_l = []
 
     for i in range((len(db_df) - 1)):
-        # if db_df.loc[i, 'm_delta'] > momentumInput and db_df.loc[i, 'sp_delta'] > spInput and db_df.loc[
-        #     i, 'fp_delta'] > fpInput:
-        # st.write(i+1, db_df.loc[i+1, 'change'])
-        if db_df.loc[i + 1, 'change_close_open'] > winInput: # and db_df.loc[i + 1, 'change_low_open'] > drawdownInput:
+        if db_df.loc[i + 1, 'change_close_open'] > winInput and db_df.loc[i + 1, 'change_low_open'] > drawdownInput:
             wins_drawdown.append(db_df.loc[i + 1, 'change_low_open'])
             w_or_l.append(1)
         else:
